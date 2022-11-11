beba0d7b6afccaaa2d21edf7fa28c92bed9f249d
WIN-5OL62GL2DHA\user <taneshasolnce8@mail.ru>
2022-11-11 18:43:04 +0300

    addit files

---
+++ script/app.js
@@ -0,0 +1,30 @@
+'use strict'
+
+/* const text = `введите текст`;
+alert (`{введенный текст}`)
+prompt (`введите текст`) */
+
+/* const userName = `имя пользователя`;
+promрt (`Ваше имя?`)
+const age = `возраст`;
+prompt (`ваш возраст?`) */
+
+/* console.log( +0 === -0) */
+/* const str = `text`;
+const dog = "Mursik";
+const hello = "hello"; */
+/* alert (`front-end`) */
+/* prompt(`where are you from`) */
+/* const whereareyoufrom = prompt(`where are you from`)
+console.loge(whereareyoufrom) */
+/* console.log(`${hello}  ${dog}`) */
+
+//const var let
+//= == ===
+//string
+//number
+//bigint
+//object
+//underfined
+//null
+//symbol
