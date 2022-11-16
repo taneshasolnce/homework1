48a465108f923a5426fd90073eb2202364f7bf73
taneshasolnce <taneshasolnce8@mail.ru>
2022-11-16 17:56:15 +0300

    fiat: edit task 1

--- script/app.js
+++ script/app.js
@@ -1,10 +1,37 @@
-'use strict'
-let hello = 'hello';
-let say = 'say';
+"use strict";
 
+// 2 занятие
+// + - / * ** % ++ -- ||  && += -= > < >= <=
+/* console.log(5 % 2);
+const num1 = 5;
+const num2 = 2;
+console.log(string(1));
+console.log(a.toString());
+console.loge(1 + "");
+const a = 1;
+console.log(1);
+// NaN false '' undefined null 0
+console.log(typeof 1); //=> 'number'
+console.log(typeof typeof false); // => 'boolean'
+
+const numOne = false;
+if (!numOne) {
+  console.log("one");
+} else {
+  console.log("else");
+}
+const numOne = promt("enter");
+if (numOne === "hello") {
+  const conf = confirm("18?");
+  if (conf === true) {
+    console.log("wow");
+  } else console.loge("1");
+}
+numOne === "hello" ? console.log("yes") : console.loge("no"); */
+//1 занятие
 /* const text = `введите текст`;
-alert (`{введенный текст}`)
-prompt (`введите текст`) */
+alert(`{введенный текст}`);
+prompt(`введите текст`); */
 
 /* const userName = `имя пользователя`;
 promрt (`Ваше имя?`)
---
+++ script/app1.js
@@ -0,0 +1,30 @@
+"use strict";
+// 1 задание
+
+/* const text = `введите текст`;
+alert(`{введенный текст}`);
+prompt(`введите текст`); */
+
+// 2 задание
+/* const num1 = +prompt(`введите первое число `, 2);
+const num2 = +prompt(`введите второе число `, 3);
+const summa = alert(`сумма=${num1 + num2}`);
+console.log(`${num1 + num2}`);
+ */
+
+// 3 задание
+/* const name = prompt(`Ваше имя?`);
+const age = prompt(`Ваш возраст?`);
+if (age >= 18) {
+  alert(`Добро пожаловать`);
+} else {
+  alert(`Простите, доступ закрыт`);
+}
+ */
+
+// 5 задание
+const num1 = +prompt(`введите первое число`);
+const num2 = +prompt(`введите второе число`);
+const num3 = +prompt(`выберите нужную арифметическую операцию + - *`);
+const summa = alert(`${num1 + num2 + num3}`);
+console.loge(`${num1 + num2 + num3}`);
